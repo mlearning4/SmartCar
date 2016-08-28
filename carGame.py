df4dc5046e51bf34ce0b923a4bdee1940045fb9f
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-08-28 10:46:01 +0530

    working on moving roads

--- carGame.py
+++ carGame.py
@@ -2,8 +2,7 @@
 # @Author: Amar Prakash Pandey
 # @Date: 25-08-2016 
 # @Email: amar.om1994@gmail.com  
-# @Github username: @amarlearning
-# @Last Modified date:  
+# @Github username: @amarlearning 
 # MIT License. You can find a copy of the License
 # @http://amarlearning.mit-license.org
 
@@ -13,8 +12,21 @@ import time
 import random
 from os import path
 
+# Material color init
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+darkBlue = (0,0,128)
+white = (255,255,255)
+black = (0,0,0)
+grey = (211,211,211)
+
 # Pygame module initialised 
 pygame.init()
+
+# Folder path init
+assets = path.join(path.dirname(__file__), 'assets')
+extras = path.join(path.dirname(__file__), 'extras')
 
 # constants are defined
 display_width = 700
@@ -26,21 +38,17 @@ grass_height = 600
 border_width = 30
 border_height = 600
 
-# Material color init
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-darkBlue = (0,0,128)
-white = (255,255,255)
-black = (0,0,0)
-grey = (211,211,211)
+divider_width = 20
+divider_height = 20
 
-# Folder path init
-assets = path.join(path.dirname(__file__), 'assets')
-extras = path.join(path.dirname(__file__), 'extras')
+block = 40
+
+# Frames per second
+FPS = 5
 
 # Init images & sounds
 gameIcon = pygame.image.load(path.join(assets + '/gameicon.png'))
+clock = pygame.time.Clock()
 
 # Game windown, caption initialised
 gameDisplay = pygame.display.set_mode((display_width, display_height))
@@ -62,7 +70,11 @@ while gameplay:
 
 
 	# Dividing th road, not the people 
-	pygame.draw.rect(gameDisplay, white, (340,20,20,100))
+	pygame.draw.rect(gameDisplay, white, ((display_width/2 - 10),20 + block,divider_width,divider_height))
+	if(block == 40):
+		block = 180
+	else:
+		block = 40
 
 	# Game basic design init [Left side]
 	pygame.draw.rect(gameDisplay, green, (0, 0, grass_width, grass_height))
@@ -74,6 +86,7 @@ while gameplay:
 
 	pygame.display.update()
 
+	clock.tick(FPS)
 
 # You will win, try one more time. Don't Quit.
 pygame.quit()
